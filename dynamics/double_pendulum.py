2c9ba89b5bb387289ce7b77cd5d7a6d14021362f
Pu Xu <ptx.pluto@gmail.com>
2022-11-05 04:20:20 +0800

    add double pendulum

---
+++ dynamics/double_pendulum.py
@@ -0,0 +1,49 @@
+import numpy as np
+from sympy import lambdify
+import inspect
+from dynamics.plottable_system import PlottableSystem
+import dynamics.eom_double_pendulum as eom
+
+
+class DoublePendulum(PlottableSystem):
+
+    def __init__(self, m1, m2, l1, l2):
+        self.l1 = l1
+        self.l2 = l2
+        self.m1 = m1
+        self.m2 = m2
+        self.g = 9.8
+        self.theta = np.array([[self.m1, self.m2, self.l1, self.l2, self.g]]).T
+        self.f = lambdify([eom.theta, eom.y], eom.eom_rhs)
+        self.x1 = lambdify([eom.theta, eom.y], eom.x1.subs(eom.rules_eom))
+        self.y1 = lambdify([eom.theta, eom.y], eom.y1.subs(eom.rules_eom))
+        self.x2 = lambdify([eom.theta, eom.y], eom.x2.subs(eom.rules_eom))
+        self.y2 = lambdify([eom.theta, eom.y], eom.y2.subs(eom.rules_eom))
+
+    def eom(self, t, y):
+        return self.f(self.theta, y.reshape(4, 1)).reshape(4, )
+
+    @property
+    def bounding_box(self):
+        padding_ratio = 1.2
+        radius = self.l1 + self.l2
+        return padding_ratio * radius * np.array([-1, 1, -1, 1])
+
+    @property
+    def ydim(self):
+        return 4
+
+    def y2pt(self, y):
+        y = y.reshape((4, 1))
+        p1 = np.array([
+            self.x1(self.theta, y),
+            self.y1(self.theta, y)
+        ])
+        p2 = np.array([
+            self.x2(self.theta, y),
+            self.y2(self.theta, y)
+        ])
+        return np.array([[0, 0], p1, p2])
+
+
+assert (inspect.isabstract(DoublePendulum) is False)
